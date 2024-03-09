dc61457d215a0cfff2d961dae96e0cf1d73ba77b
Ankit <91782014+itsankitkgupta@users.noreply.github.com>
2024-03-09 16:50:28 +0530

    Update Reservation.jsx

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -17,7 +17,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://gorgeous-boa-sundress.cyclic.app//api/v1/reservation/send",
+        "https://gorgeous-boa-sundress.cyclic.app/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
